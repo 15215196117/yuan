df95bfdc385b0eab4d2f6c7f05a118fe3fd6258b
young <a1352794122@gmail.com>
2017-11-16 18:50:11 +0800

    Update rabbitmq.sh

--- mylinux/常用软件/rabbitmq.sh
+++ mylinux/常用软件/rabbitmq.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+rabbitmqctl add_user juhefu "password"  //新增用户juhefu
+rabbitmqctl set_user_tags juhefu administrator  //设置用户tag
+ rabbitmqctl help  set_permissions 
+rabbitmqctl set_permissions -p / juhefu '.*' '.*' '.*'  //设置用户权限，这里设置/(根目录) 所有权限
