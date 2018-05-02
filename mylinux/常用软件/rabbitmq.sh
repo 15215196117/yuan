8dda205c49c81d99144069b43ee76c9228932301
young <a1352794122@gmail.com>
2018-05-02 23:32:37 +0800

    Update rabbitmq.sh

--- mylinux/常用软件/rabbitmq.sh
+++ mylinux/常用软件/rabbitmq.sh
@@ -4,3 +4,6 @@ rabbitmqctl add_user juhefu "password"  //新增用户juhefu
 rabbitmqctl set_user_tags juhefu administrator  //设置用户tag
  rabbitmqctl help  set_permissions 
 rabbitmqctl set_permissions -p / juhefu '.*' '.*' '.*'  //设置用户权限，这里设置/(根目录) 所有权限
+
+rabbitmq-plugins enable rabbitmq_management  //开启web管理界面 rabbitmq-plugins版本不同不一定在bin目录下面
+
